9cab56f034af7741cbf536f818e463a87527898d
Styrr <st.d.koehler@gmx.de>
2025-04-25 00:43:48 +0200

    add text to speech streaming

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-""" FastAPI Server"""
+"""FastAPI Server"""
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers.interaction import router as interaction_router
 from src.routers.mission import router as session_router
+from src.routers.tts import router as tts_router
 
 # uvicorn main:app --host 0.0.0.0 --port 8000 --workers 2
 # https://www.vidavolta.io/streaming-with-fastapi/
@@ -22,3 +23,4 @@ app.add_middleware(
 
 app.include_router(interaction_router)
 app.include_router(session_router)
+app.include_router(tts_router)
---
+++ src/routers/tts.py
@@ -0,0 +1,75 @@
+"""endpoints calling session"""
+
+from collections.abc import Generator
+import requests
+import io
+
+from pydantic import BaseModel
+from fastapi import APIRouter
+from fastapi.responses import StreamingResponse
+
+from src.utils.logger import configure_logger
+
+
+log = configure_logger("mission")
+
+router = APIRouter(
+    prefix="/tts",
+    tags=["tts"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+class TtsRequest(BaseModel):
+    text: str
+
+
+@router.post("/tts")
+def tts(request: TtsRequest) -> StreamingResponse:
+    """
+    Forward the generated MP3 file to the client using streaming.
+    """
+    text = request.text.split("---")[0]
+    response = requests.post(
+        "http://127.0.0.1:8001/inference/text-to-speech",
+        json={"model": "JoeyCocoDiaz", "text": text},
+        timeout=360,
+    )
+
+    # Ensure the response content is in bytes and wrap it in a BytesIO stream
+    mp3_stream = io.BytesIO(response.content)
+
+    return StreamingResponse(
+        mp3_stream,  # Stream the content
+        media_type="audio/mpeg",  # MIME type for MP3
+        headers={"Content-Disposition": "attachment; filename=inference.mp3"},
+    )
+
+
+@router.post("/tts-stream")
+def tts_stream(request: TtsRequest) -> StreamingResponse:
+    """
+    Forward the upstream streaming MP3 to the client in real time.
+    """
+
+    def generate_audio() -> Generator[bytes]:
+        i = 0
+        text = request.text.split("---")[0]
+        with requests.post(
+            "http://127.0.0.1:8001/inference/text-to-speech-stream-webm",
+            json={"model": "JoeyCocoDiaz", "text": text},
+            timeout=360,
+            stream=True,
+        ) as r:
+            r.raise_for_status()
+            for chunk in r.iter_content(chunk_size=4096):
+                if chunk:
+                    i += 1
+                    print(i)
+                    yield chunk
+
+    return StreamingResponse(
+        generate_audio(),
+        media_type="audio/webm;codecs=opus",
+        headers={"Content-Disposition": "attachment; filename=inference.webm"},
+    )
